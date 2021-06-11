9eb435a21d0d80fc7522f431f53f3a93c8b57d6e
nishant-batra <nishantbatra2000@gmail.com>
2021-06-11 17:18:32 +0530

    install dependecies and data

---
+++ src/lib/firebase.js
@@ -0,0 +1,17 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+//import { seedDatabase } from "../seed";
+const config = {
+  apiKey: "AIzaSyCt9WgTzuizjfhQjUJDEWq06QVP346h2ik",
+  authDomain: "login-ea971.firebaseapp.com",
+  projectId: "login-ea971",
+  storageBucket: "login-ea971.appspot.com",
+  messagingSenderId: "479314112973",
+  appId: "1:479314112973:web:27d28f69fd28d7476d9484",
+  measurementId: "G-7QL5V6ZR62",
+};
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+//seedDatabase(firebase);
+export { firebase, FieldValue };
