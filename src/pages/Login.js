1c70ef2d2be4e17cb24ce087a0afbfe7023ccfbe
nishant-batra <nishantbatra2000@gmail.com>
2021-06-13 17:48:23 +0530

    add Login UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 function Login(props) {
   const history = useHistory();
@@ -14,7 +14,62 @@ function Login(props) {
   }, []);
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      This Is login Page!!.{" "}
+      <div className="flex w-3/5">
+        <img
+          src="../../images/iphone-with-profile.jpg"
+          alt="iphone with login image"
+        />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img
+              src="/images/logo.png"
+              alt="gram"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your Email Address"
+              type="email"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => {
+                setEmailAdress(target.value);
+              }}
+            />
+            <input
+              aria-label="Enter your Password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => {
+                setPassword(target.value);
+              }}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+                isInvalid && "opacity-50"
+              }`}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary rounded">
+          <p className="text-sm">
+            Don't have and account? {` `}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              {" "}
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
